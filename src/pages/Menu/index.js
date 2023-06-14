14ffc17409d58eea016b5b1c2a2c24decddd3abf
luizlopeslpc28 <luizalcorreia28@gmail.com>
2023-06-14 19:15:58 -0300

    ALTERAÇÃO NO RECUPERAR SENHA

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -4,41 +4,65 @@ import { Text, Card, Button, Icon } from 'react-native-elements';
 
 import { useNavigation } from '@react-navigation/native';
 
-export default function Menu(){
-
-    const navigation = useNavigation();
+export default function Menu() {
+  const navigation = useNavigation();
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.container}>
-            <TouchableOpacity onPress={() => navigation.navigate('Chamados')}>
-                <Card>
-                    <Card.Title>CHAMADOS</Card.Title>
-                    <Card.Divider />
-                    <Card.Image
-                        style={{ padding: 0 }}
-                        source={require('../../assets/bloco-de-anotacoes.png')}
-                    />
-                    <Text style={{ marginBottom: 10, alignItems: 'center', justifyContent:'center' }}>
-                        Crie ou acompanhe seus chamados
-                    </Text>
-                    
-                </Card>
-            </TouchableOpacity>    
+          <TouchableOpacity onPress={() => navigation.navigate('Chamados')}>
+            <Card containerStyle={styles.cardContainer}>
+              <Card.Title style={styles.cardTitle}>CHAMADOS</Card.Title>
+              <Card.Divider />
+              <View style={styles.imageContainer}>
+                <Card.Image
+                  style={styles.cardImage}
+                  source={require('../../assets/bloco-de-anotacoes.png')}
+                />
+              </View>
+              <Text style={styles.cardText}>
+                Crie ou acompanhe seus chamados
+              </Text>
+            </Card>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
-    );
-}  
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: 10,
-      backgroundColor: '#ecf0f1',
-    },
-  });
+  container: {
+    flex: 1,
+    paddingTop: 10,
+    backgroundColor: '#131549',
+  },
+  cardContainer: {
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  cardTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  imageContainer: {
+    alignItems: 'center',
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  cardImage: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+  },
+  cardText: {
+    marginBottom: 10,
+    textAlign: 'center',
+    color: '#6e6e6e',
+  },
+});
+
 
 
   /*
@@ -54,4 +78,4 @@ const styles = StyleSheet.create({
               title="RESERVE AGORA"
               onPress={() => {alert('Reservado!')}}
             />
-*/
+*/ 
--- src/pages/RecuperarSenha/index.js
+++ src/pages/RecuperarSenha/index.js
@@ -1,81 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 
-import {useNavigation} from '@react-navigation/native'
+export default function RecuperarSenha() {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-export default function RecuperarSenha(){
-
-    const navigation = useNavigation();
-
-    function recuperar(){
-        alert('Uma nova senha foi enviada ao seu e-mail cadastrado');
-        navigation.navigate('Login')
+  function recuperar() {
+    if (newPassword !== confirmPassword) {
+      alert('As senhas não coincidem. Por favor, tente novamente.');
+      return;
     }
 
-    return(
-        <View style={styles.container}>
-            <Text style={styles.title}>Recuperar Senha</Text>
+    const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ password: newPassword })
+    };
 
-            <View style={styles.inputArea}>
-                <Text style={styles.inputText}>E-mail:</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="name@example.com"
-                />
-            </View>
+    fetch(`http://localhost:8080/users/alterar/${email}`, requestOptions)
+      .then(response => {
+        if (response.ok) {
+          alert('Senha do usuário atualizada com sucesso!');
+          navigation.navigate('Login');
+        } else {
+          throw new Error('Erro ao atualizar a senha do usuário');
+        }
+      })
+      .catch(error => {
+        console.error('Erro ao atualizar a senha do usuário:', error);
+        alert('Erro ao atualizar a senha do usuário. Por favor, tente novamente mais tarde.');
+      });
+  }
 
-            <View style={styles.btnArea}>
-                <TouchableOpacity style={styles.btn} onPress={recuperar}>
-                    <Text style={styles.btnText}>Recuperar senha</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Recuperar Senha</Text>
+
+      <View style={styles.inputArea}>
+        <Text style={styles.inputText}>E-mail:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="name@example.com"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={styles.inputArea}>
+        <Text style={styles.inputText}>Nova Senha:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nova Senha"
+          secureTextEntry
+          value={newPassword}
+          onChangeText={setNewPassword}
+        />
+      </View>
+      <View style={styles.inputArea}>
+        <Text style={styles.inputText}>Confirmar Senha:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Confirmar Senha"
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+      </View>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn} onPress={recuperar}>
+          <Text style={styles.btnText}>Recuperar Senha</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#131549'
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 25,
-        color: '#FFF',
-        padding: 30,
-        marginTop: 230
-    },
-    inputArea:{
-        padding: 60
-    },
-    inputText:{
-        color: '#FFF',
-        fontSize: 18
-    },
-    input: {
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#FFF',
-        padding: 7,
-        paddingTop: 5,
-        borderRadius: 8,
-        marginTop: 10
-    },
-    btnArea:{
-        width: '50%',
-        height: '5%'
-    },
-    btn:{
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#1e90ff',
-        borderRadius: 10
-    },
-    btnText:{
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: "#FFF"
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#131549'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    color: '#FFF',
+    marginTop: 80,
+    marginBottom: 30
+  },
+  inputArea: {
+    marginBottom: 20,
+    width: '80%',
+  },
+  inputText: {
+    color: '#FFF',
+    fontSize: 18,
+    marginBottom: 5
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#FFF',
+    padding: 10,
+    borderRadius: 8,
+  },
+  btnArea: {
+    width: '50%',
+    height: '5%',
+  },
+  btn: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1e90ff',
+    borderRadius: 10
+  },
+  btnText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: "#FFF"
+  }
+});
